7a468849636944a8ad1ba6a851ba1c49c3c10b77
Diaa <deiaa_x@yahoo.com>
2021-01-19 04:21:35 +0200

    fix error in index.js file and add process.env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
-if (env === 'production') {
+if (process.env === 'production') {
   console.log = function () {};
 }
 ReactDOM.render(
