74bb1f12034f1150c09c71b7951cdc34692844c7
Diaa <deiaa_x@yahoo.com>
2021-01-19 07:21:08 +0200

    fix console with netliify

--- src/App/util/ConsoleHelper.js
+++ src/App/util/ConsoleHelper.js
@@ -1,4 +1,4 @@
-if (process.env.GATSBY_NODE_ENV !== 'development') {
+if (process.env.GATSBY_NODE_ENV === 'production') {
   console.log('this node env now in production');
   console.log = function () {};
 }
